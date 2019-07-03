6c2990fc4fd558d9e8a849d5f3bb08be10937a7f
painarniano@gmail.com <Gilgamesh555>
2019-07-03 19:08:32 -0400

    no da tamare

--- crud/routes/index.js
+++ crud/routes/index.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+var COMPRADOR = require('../database/users');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.status(200).json({
     msn: "GAAA"
   })
 });
-
+router.post('/comprador', async(req, res)=> {
+  var params = req.body;
+  params["registerDate"] = new Date();
+  var compradores = new COMPRADOR(params);
+  var result = await compradores.save();
+  res.status(200).json(result);
+});
+router.get('/comprador', (req, res)=>{
+  COMPRADOR.find({}, (err, docs) => {
+    res.status(200).json(docs);
+  });
+});
 module.exports = router;
