f3853aed71b33399c1476a0c96949ed0b2a2926e
PintoFernando <ghomasricas@gmail.com>
2019-07-03 18:50:34 -0400

    Inicio, falta algunos cambios

--- app/src/main/java/com/example/apptiendamobil/MainActivity.java
+++ app/src/main/java/com/example/apptiendamobil/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.apptiendamobil;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+   // private TextView texto1;
+
+    //private Typeface kaibon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hola es una pruebaxdxdxd
+
+        //String fuente1 = "fuentes/kaibon.otf";
+        //this.kaibon= Typeface.createFromAsset(getAssets(),fuente1);
+        //texto1 = (TextView) findViewById(R.id.txt2);
+        //texto1.setTypeface(kaibon);
+
 
     }
 
-    @Override
-    public void onProvideAssistData(Bundle data) {
-        super.onProvideAssistData(data);
-    }
+
 }
