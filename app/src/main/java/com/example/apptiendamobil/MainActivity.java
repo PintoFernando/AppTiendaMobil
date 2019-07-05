2f417929a16b211e89efb0237c861e04c3044ccb
HidibertoSecko <hidibertosecko@gmail.com>
2019-07-04 21:44:07 -0400

    ventana principal y menu, primera prueba

--- app/src/main/java/com/example/apptiendamobil/MainActivity.java
+++ app/src/main/java/com/example/apptiendamobil/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.apptiendamobil;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //hola es una pruebaxdxdxd
+    }
+
+    public void siguiente(View view) {
+        Intent inicio = new Intent(this, Principal.class);
+        startActivity(inicio);
 
     }
 
