45dde414ecb5b540e88be154aa679a441e0ba437
alejandro <aledar16@gmail.com>
2019-07-06 02:05:58 -0400

    Pare Registrarse

--- app/src/main/java/com/example/apptiendamobil/login.java
+++ app/src/main/java/com/example/apptiendamobil/login.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
@@ -14,13 +15,24 @@ public class login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Button otro;
+        TextView Ir_Registro;
         otro = (Button)findViewById(R.id.btn2);
         otro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent otro = new Intent(login.this,menu.class);
+                Intent otro = new Intent(login.this,Principal.class);
                 startActivity(otro);
             }
         });
+
+        Ir_Registro = (TextView) findViewById(R.id.registrate);
+        Ir_Registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Ir_Registro = new Intent(login.this,Principal.class);
+                startActivity(Ir_Registro);
+            }
+        });
+
     }
 }
